f54e89ff9d8670a2b797db9a425918712d6609c8
Sushrut004 <91841361+Sushrut004@users.noreply.github.com>
2021-10-25 15:06:45 +0530

    Add files via upload

---
+++ project_switchcase1.m
@@ -0,0 +1,10 @@
+classdef project_switchcase1<Simulink.IntEnumType
+    enumeration
+        Forward_backward_Motion(1)
+        Seat_Height_up_down(2)
+        Headrest(3)
+        seat_recliner(4)
+        thigh_support(5)
+    end
+end
+        
