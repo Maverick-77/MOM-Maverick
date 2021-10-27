83e8ebf1892f03a35365965d1c6a5ef4062414e0
Maverick-77 <91833086+Maverick-77@users.noreply.github.com>
2021-10-27 23:23:53 +0530

    Add files via upload
    Full Feature Model with speed control

---
+++ Full Feature Model/Seat_PC_System.m
@@ -0,0 +1,10 @@
+classdef Seat_PC_System<Simulink.IntEnumType
+    enumeration
+        Forward_Backward_M(1)
+        Hight_Upward_Downward_M(2)
+        Headrest_Upward_Downward_M(3)
+        SeatBack_FrontTilt_RearTilt_Recline_M(4)
+        Thigh_Support_Upward_Downward_M(5)
+        Lumbar_Support_On_Off(6)
+    end
+end
